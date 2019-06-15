fbb804fbd8b1966feb2e3e7d9990d23cb2c0ba2e
JesusJoey <joeyang@bu.edu>
2019-06-16 00:06:59 +0800

    update June 16th

--- Permutations.java
+++ Permutations.java
@@ -79,3 +79,67 @@ public class Solution {
 		}
 	}
 }
+
+/**
+39. Combination Sum
+Input: candidates = [2,3,6,7], target = 7,
+A solution set is:
+[
+  [7],
+  [2,2,3]
+]
+*/
+class Solution {
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, target, 0);
+        return list;
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
+        if(remain < 0) return;
+        else if(remain == 0) list.add(new ArrayList<>(tempList));
+        else{ 
+            for(int i = start; i < nums.length; i++){
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums, remain - nums[i], i); 
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+}
+
+/**
+40. Combination Sum II
+Input: candidates = [10,1,2,7,6,1,5], target = 8,
+A solution set is:
+[
+  [1, 7],
+  [1, 2, 5],
+  [2, 6],
+  [1, 1, 6]
+]
+*/
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, target, 0);
+        return list;
+    }
+    
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums,int remain, int start) {
+        if (remain < 0) return;
+        else if (remain == 0) 
+            list.add(new ArrayList<> (tempList));
+        else {
+            for (int i = start; i < nums.length; i++) {
+                if (i > start && nums[i] == nums[i-1]) continue;
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums, remain - nums[i], i+1); 
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+}  
